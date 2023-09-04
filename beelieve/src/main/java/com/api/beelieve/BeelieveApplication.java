bf7cd8fcefcc9bcaa45d56297dd6e21d3b9807cd
Bruno Denardo <brunoden.bd@gmail.com>
2023-09-04 11:50:10 -0300

    Início do projeto com Spring Boot

---
+++ beelieve/src/main/java/com/api/beelieve/BeelieveApplication.java
@@ -0,0 +1,13 @@
+package com.api.beelieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeelieveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeelieveApplication.class, args);
+	}
+
+}
---
+++ beelieve/src/test/java/com/api/beelieve/BeelieveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.beelieve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeelieveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
