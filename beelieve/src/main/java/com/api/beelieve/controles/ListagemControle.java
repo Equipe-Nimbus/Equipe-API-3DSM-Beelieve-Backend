6e5605166c2fd7819de5f4ea2fae7df96648e16f
acorreac <aline.cristianecorrea@hotmail.com>
2023-09-10 20:24:00 -0300

    listagens dos subproejtos e tarefas

--- beelieve/src/main/java/com/api/beelieve/controles/ListagemControle.java
+++ beelieve/src/main/java/com/api/beelieve/controles/ListagemControle.java
@@ -9,18 +9,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.beelieve.entidades.Projeto;
+import com.api.beelieve.entidades.SubProjeto;
+import com.api.beelieve.entidades.Tarefa;
+import com.api.beelieve.entidades.tipoProjeto;
+import com.api.beelieve.repositorio.ModuloSubProjetoRepositorio;
 import com.api.beelieve.repositorio.ProjetoRepositorio;
+import com.api.beelieve.repositorio.SubProjetoRepositorio;
+import com.api.beelieve.repositorio.TarefaRepositorio;
 
 @RestController
-@RequestMapping("/listaProjetos")
+@RequestMapping("/listagens")
 public class ListagemControle {
 	
 	@Autowired
 	private ProjetoRepositorio projetoRepositorio;
 	
-	@GetMapping("/projeto")
+	@Autowired
+	private SubProjetoRepositorio subProjetoRepositorio;
+	
+	@Autowired
+	private TarefaRepositorio tarefaRepositorio;
+	
+	@GetMapping("/projetos")
 	public ResponseEntity<List<Projeto>> ListarProjeto() {
-		var lista = projetoRepositorio.findAll();
-		return ResponseEntity.ok(lista);
+		var listaProjeto = projetoRepositorio.findAll();
+		return ResponseEntity.ok(listaProjeto);
 	}
+	
+	@GetMapping("/subprojetos")
+	public ResponseEntity<List<SubProjeto>> ListarSubProjeto() {
+		var listaSubProjeto = subProjetoRepositorio.findAll();
+		return ResponseEntity.ok(listaSubProjeto);
+	}
+	
+	@GetMapping("/tarefas")
+	public ResponseEntity<List<Tarefa>> ListarTarefa() {
+		var listaTarefa = tarefaRepositorio.findAll();
+		return ResponseEntity.ok(listaTarefa);
+	}
+	
 }
