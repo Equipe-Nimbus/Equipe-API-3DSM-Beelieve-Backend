b2d2296a2841073c9abb03e2b1f57fca0cc74722
Bruno Denardo <brunoden.bd@gmail.com>
2023-10-07 21:16:42 -0300

    (fix) persistencia do progresso real no MongoDB concertada

--- beelieve/src/main/java/com/api/beelieve/entidades/cronograma/servico/AtualizaProgressoRealCronograma.java
+++ beelieve/src/main/java/com/api/beelieve/entidades/cronograma/servico/AtualizaProgressoRealCronograma.java
@@ -28,8 +28,7 @@ public class AtualizaProgressoRealCronograma {
 		DataCustomizada dataAtual = data_repositorio.findById(Long.valueOf(1)).get();
 		Calendar calendario = Calendar.getInstance();
 		calendario.setTime(dataAtual.getData());
-		int numeroMes = calendario.MONTH + 1;
-		
+		int numeroMes = calendario.get(calendario.MONTH) + 1;
 		String mesProgresso = mesesCronograma.converteNumeroMes(numeroMes);
 		
 		cronograma.getLista_cronograma().forEach((mes)->{
@@ -37,12 +36,11 @@ public class AtualizaProgressoRealCronograma {
 				mes.getNiveis().forEach((entidade)->{
 					if(entidade.getId_nivel() == id_entidade && entidade.getTipo().equals(tipo_entidade)) {
 						entidade.setProgresso_real(progresso_real);
+					
 					}
 				});
 			}
 		});
-		
 		cronograma_repositorio.save(cronograma);
 	}
-		
 }
