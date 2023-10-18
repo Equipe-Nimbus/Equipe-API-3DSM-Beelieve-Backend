6116e867554d1841ee670f1970d02d234595eb99
acorreac <aline.cristianecorrea@hotmail.com>
2023-10-18 10:38:36 -0300

    feat: Rotas de listagens do usuario

--- beelieve/src/main/java/com/api/beelieve/controles/ControleUsuario.java
+++ beelieve/src/main/java/com/api/beelieve/controles/ControleUsuario.java
@@ -1,9 +1,17 @@
 package com.api.beelieve.controles;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.beelieve.entidades.usuario.Usuario;
+import com.api.beelieve.entidades.usuario.dto.DadosListagemUsuario;
+import com.api.beelieve.entidades.usuario.servico.ListaUsuarioGeral;
 import com.api.beelieve.repositorio.UsuarioRepositorio;
 
 @RestController
@@ -11,4 +19,23 @@ import com.api.beelieve.repositorio.UsuarioRepositorio;
 public class ControleUsuario {
 	@Autowired
 	private UsuarioRepositorio repositorio_usuario;
+	
+	@Autowired
+	private ListaUsuarioGeral listaUsuarioGeral;
+	
+	
+	@GetMapping("/listar")
+	public ResponseEntity<List<DadosListagemUsuario>> listar() {
+		List<Usuario> listaUsuario = repositorio_usuario.findAll();
+		List<DadosListagemUsuario> listaUsuarioModificada = listaUsuarioGeral.listarUsuarios(listaUsuario);
+		System.out.println(listaUsuarioModificada);
+		return ResponseEntity.ok(listaUsuarioModificada);
+	}
+	
+	@GetMapping("/listar/{id}")
+	public ResponseEntity<DadosListagemUsuario> listarId(@PathVariable Long id){
+		DadosListagemUsuario usuario = repositorio_usuario.acharUsuario(id);
+		return ResponseEntity.ok(usuario);
+	};
+	
 }
