1741261f71d706e514fc4ae78ad1a067ee59ff37
Bruno Denardo <brunoden.bd@gmail.com>
2023-11-08 20:28:01 -0300

    fix: Corringindo progresso do cronograma

--- beelieve/src/main/java/com/api/beelieve/BeelieveApplication.java
+++ beelieve/src/main/java/com/api/beelieve/BeelieveApplication.java
@@ -1,9 +1,7 @@
 package com.api.beelieve;
 
 
-import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
--- beelieve/src/main/java/com/api/beelieve/entidades/cronograma/servico/AtualizaProgressoRealCronograma.java
+++ beelieve/src/main/java/com/api/beelieve/entidades/cronograma/servico/AtualizaProgressoRealCronograma.java
@@ -1,5 +1,6 @@
 package com.api.beelieve.entidades.cronograma.servico;
 
+import java.text.BreakIterator;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -25,6 +26,8 @@ public class AtualizaProgressoRealCronograma {
 	private DataAtualAplicacao dataAtualAplicacao;
 	
 	private int diferencaAno;
+	private Boolean alteracaoJaFeita = false;
+	private int ordem_mes_cronograma = 0;
 
 	public void atualizarProgressoCronograma(Long id_projeto, String tipo_entidade, Long id_entidade, Double progresso_real) {
 		diferencaAno = 0;
@@ -33,20 +36,34 @@ public class AtualizaProgressoRealCronograma {
 		Calendar calendarioAtual = Calendar.getInstance();
 		calendarioInicial.setTime(cronograma.getInicio_projeto());
 		calendarioAtual.setTime(dataAtualAplicacao.data);
-		int numeroMes = calendarioAtual.get(calendarioAtual.MONTH) + 1;
-		String mesProgresso = mesesCronograma.converteNumeroMes(numeroMes);
+		int numeroMesAtual = calendarioAtual.get(calendarioAtual.MONTH) + 1;
+		int numeroMesInicial = calendarioInicial.get(calendarioInicial.MONTH);
+
+		
+		System.out.println("numeroMesAtual: " + numeroMesAtual);
+		System.out.println("numeroMesInicial" + numeroMesInicial);
+		
 		
 		diferencaAno = Math.abs(calendarioAtual.get(calendarioAtual.YEAR) - calendarioInicial.get(calendarioInicial.YEAR));
 		
+		
+		System.out.println("Diferenca ano: " + diferencaAno);
+		
+		
+		System.out.println("Ordem do mes corrigida: " + ((numeroMesAtual - numeroMesInicial) + (12 * diferencaAno)));
+		ordem_mes_cronograma = (numeroMesAtual - numeroMesInicial) + (12 * diferencaAno);
+		alteracaoJaFeita = false;
 		cronograma.getLista_cronograma().forEach((mes)->{
-			if(mesProgresso.equals(mes.getMes_cronograma())) {
+			if(mes.getOrdem_mes_cronograma() == ordem_mes_cronograma || alteracaoJaFeita) {
 				mes.getNiveis().forEach((entidade)->{
 					if(entidade.getId_nivel() == id_entidade && entidade.getTipo().equals(tipo_entidade)) {
-						if(diferencaAno == 0) {
+						if(!alteracaoJaFeita) {
+							System.out.println("Foooi");
 							entidade.setProgresso_real(progresso_real);
+							alteracaoJaFeita = true;
 						}
-						else {
-							diferencaAno -= 1;
+						else{
+							entidade.setProgresso_real(progresso_real);
 						}
 					}
 				});
@@ -54,4 +71,7 @@ public class AtualizaProgressoRealCronograma {
 		});
 		cronograma_repositorio.save(cronograma);
 	}
+	
+
+	
 }
