fa3313e351b85f29dd167e803e03cdaae8c7ade1
Bruno Denardo <brunoden.bd@gmail.com>
2023-11-17 08:29:54 -0300

    rebase: resolucao de mais conflitos do rebase

--- beelieve/src/main/java/com/api/beelieve/configuracoes/seguranca/SegurancaConfig.java
+++ beelieve/src/main/java/com/api/beelieve/configuracoes/seguranca/SegurancaConfig.java
@@ -1,7 +1,9 @@
 package com.api.beelieve.configuracoes.seguranca;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,16 +12,25 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SegurancaConfig {
+	
+	@Autowired
+	private FiltroSeguranca filtro;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-		return http.csrf(csrf -> csrf.disable())
-				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.build();
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    return http.csrf(csrf -> csrf.disable())
+	            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	            .authorizeHttpRequests(req -> {
+	                req.requestMatchers(HttpMethod.POST, "/usuario/login").permitAll();
+	                req.anyRequest().authenticated();
+	            })
+	            .addFilterBefore(filtro, UsernamePasswordAuthenticationFilter.class)
+	            .build();
 	}
 	
 	@Bean
--- beelieve/src/main/java/com/api/beelieve/configuracoes/seguranca/ServicoToken.java
+++ beelieve/src/main/java/com/api/beelieve/configuracoes/seguranca/ServicoToken.java
@@ -10,6 +10,7 @@ import com.api.beelieve.entidades.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 @Service
 public class ServicoToken {
@@ -26,6 +27,21 @@ public class ServicoToken {
 		        .sign(algoritimo);
 		} catch (JWTCreationException exception){
 		    throw new RuntimeException("Não foi possível gerar o token", exception);
+		}
+	}
+	
+	public String validaToken(String tokenJWT) {
+		try {
+		    Algorithm algorithm = Algorithm.HMAC256("senhaToken");
+		    return JWT.require(algorithm)
+		        .withIssuer("beelieve")
+		        .build()
+		        .verify(tokenJWT)
+		        .getSubject();
+		        
+
+		} catch (JWTVerificationException exception){
+		    throw new RuntimeException("Token invalido ou expirado", exception);
 		}
 	}
 	
