d39f7ad3e38e766164787c5cc290d1791aa1fa2c
acorreac <aline.cristianecorrea@hotmail.com>
2023-11-21 23:40:46 -0300

    feat: delete relação analista e projeto ao excluir projeto

---
+++ beelieve/src/main/java/com/api/beelieve/entidades/analista_projeto/servico/DeleteAtribuicaoAnalista.java
@@ -0,0 +1,30 @@
+package com.api.beelieve.entidades.analista_projeto.servico;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.beelieve.entidades.analista_projeto.Analista_Projeto;
+import com.api.beelieve.entidades.projeto.Projeto;
+import com.api.beelieve.repositorio.Analista_ProjetoRepositorio;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class DeleteAtribuicaoAnalista {
+	
+	@Autowired
+	private Analista_ProjetoRepositorio repositorio_relacao_analista;
+	
+	
+	@Transactional
+	public void deleteAtribuicao(Projeto projeto) {
+		List<Analista_Projeto> analistas = projeto.getAnalistasAtribuidos();
+		
+		analistas.forEach((analistaAtribuido)->{
+			Long relaçãoId = analistaAtribuido.getId_relacao_analista_projeto();
+			repositorio_relacao_analista.deleteById(relaçãoId);
+		});
+	}
+}
